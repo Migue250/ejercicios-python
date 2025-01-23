c3bd5c255d0e9a63af66c54e9fa712b85e0dd09d
migue250 <miguel.gordo.martinez@gmail.com>
2025-01-23 12:20:50 +0100

    Prueba kraken

---
+++ basicos/Funciones/ejercicio1.py
@@ -0,0 +1,21 @@
+
+def saludo():
+    print (f"hola mundo")
+
+saludo()
+
+def saludar(nombre):
+  print(f"hola mister {nombre} toenes {edad}")
+
+  saludar("Marco",25)
+  
+  def saludoPideNombre():
+    nombre=input("dame un nombre")
+    print(f"Hola {nombre}")
+   
+
+    saludoPideNombre()
+
+    def suma (num1, num2):
+        total = num1+num2
+        return total
--- ciudades.py
+++
@@ -1,8 +0,0 @@
-
-
-nombre = "Juan"  
-edad = 25        
-ciudad = "Madrid"  
-
-
-print(f"Hola, mi nombre es {nombre}, tengo {edad} años y vivo en {ciudad}.")
