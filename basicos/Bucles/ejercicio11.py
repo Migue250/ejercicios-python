f982a48848f5449f44313fe36de8f6841fc0444a
migue250 <miguel.gordo.martinez@gmail.com>
2025-02-06 20:49:11 +0100

    nuevos ejercicios

---
+++ basicos/Bucles/ejercicio11.py
@@ -0,0 +1,7 @@
+numero = 5
+
+salida = 0
+for i in range(10):
+   salida = i  * numero
+   print (salida)
+ 
---
+++ basicos/Bucles/ejercicio9.py
@@ -0,0 +1,5 @@
+cadena = "Hola a todos"
+
+cadenasinespacios = cadena.replace(" ", "")
+print("Cadena sin espacios:", cadenasinespacios)
+
